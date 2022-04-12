ad5a2af589742eca57218024125ad7089e7c9a82
AyoCodess <ayo.daniel.adesanya@gmail.com>
2022-04-12 14:04:14 +0800

    implemented base api call

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import AppContainer from './components/AppContainer';
 import List from './components/List';
 import Navbar from './components/Navbar';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const apiCall = async () => {
+      try {
+        const response = await axios(
+          `https://restcountries.com/v2/all?fields=name,region,area`
+        );
+
+        setData(response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    apiCall();
+  }, []);
+
   return (
     <>
       <Navbar />
       <AppContainer>
-        <List />
+        <List data={data} />
       </AppContainer>
     </>
   );
--- src/components/List.js
+++ src/components/List.js
@@ -11,27 +11,36 @@ const people = [
   // More people...
 ];
 
-function List() {
+function List({ data }) {
+  console.log({ data });
   return (
     <div className='grid grid-cols-1 gap-4 mt-5 '>
-      {people.map((person) => (
+      {data.map((country) => (
         <div
-          key={person.email}
-          className='relative rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm flex items-center space-x-3 hover:border-gray-400 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500'>
-          <div className='flex-shrink-0'>
-            <img
-              className='h-10 w-10 rounded-full'
-              src={person.imageUrl}
-              alt=''
-            />
+          key={country.name}
+          className=' rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm flex items-center justify-center space-x-10 hover:border-gray-400 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500'>
+          <div className=' w-48'>
+            <div className=' text-md '>{country.name}</div>
           </div>
-          <div className='flex-1 min-w-0'>
-            <a href='#' className='focus:outline-none'>
-              <span className='absolute inset-0' aria-hidden='true' />
-              <p className='text-sm font-medium text-gray-900'>{person.name}</p>
-              <p className='text-sm text-gray-500 truncate'>{person.role}</p>
-            </a>
+          <div className='w-48'>
+            <p className='text-sm font-medium text-gray-900'>
+              Region: {country.region}
+            </p>
+            <p className='text-sm text-gray-500 truncate'>
+              Area Size: {country.area}
+            </p>
           </div>
+          {/* // - data is not useful */}
+          {/* <div className='text-sm'>
+            {country.independent === false && (
+              <span className='text-red-500'>
+                Not <br /> Independant
+              </span>
+            )}
+            {country.independent === true && (
+              <span className='text-green-500'>'Independant'</span>
+            )}
+          </div> */}
         </div>
       ))}
     </div>
