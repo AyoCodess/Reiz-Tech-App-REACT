5f52751dea19f8a8c0d5e3a385faabb4ec420abc
AyoCodess <ayo.daniel.adesanya@gmail.com>
2022-04-13 18:08:13 +0800

    small UI updates

--- src/components/List.js
+++ src/components/List.js
@@ -32,11 +32,11 @@ function List({
           <hr className='border w-2/3 border-gray-200 mx-auto' />
         </>
       )}
-
+      <div className='text-xs text-gray-400'>List of Countries </div>
       {data.map((country) => (
         <div
           key={country.name}
-          className=' rounded-lg shadow border border-gray-300 bg-white px-6 py-3  flex items-center justify-center space-x-10 hover:border-gray-400 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500'>
+          className=' rounded-lg shadow border border-gray-300 bg-white px-6 py-3  flex items-center justify-center space-x-10'>
           <div className=' w-48'>
             <div className=' text-md '>{country.name}</div>
           </div>
