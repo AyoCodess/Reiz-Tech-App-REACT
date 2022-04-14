fbf19611e67291fdb7d1396bfe18eaa57b9cc1cd
AyoCodess <ayo.daniel.adesanya@gmail.com>
2022-04-14 17:59:42 +0800

    updated on UI for larger screens

--- src/components/List.js
+++ src/components/List.js
@@ -18,7 +18,7 @@ function List({
     );
 
   return (
-    <div className='grid grid-cols-1 gap-4 mt-2  '>
+    <div className='grid grid-cols-1 gap-4 mt-2 '>
       {results && (
         <>
           {!resultData && (
@@ -41,15 +41,15 @@ function List({
           <hr className='border w-2/3 border-gray-200 mx-auto' />
         </>
       )}
-      <div className='text-xs text-gray-400'>List of Countries </div>
+      <div className='text-xs text-gray-400 '>List of Countries </div>
       {data.map((country) => (
         <div
           key={country.name}
           className=' rounded-lg shadow border border-gray-300 bg-white px-6 py-3  flex items-center justify-center space-x-10'>
-          <div className=' w-48'>
+          <div className=' w-96 mx-auto'>
             <div className=' text-md '>{country.name}</div>
           </div>
-          <div className='w-48'>
+          <div className='w-96 mx-auto'>
             <p className='text-sm font-medium text-gray-900'>
               Region: {country.region}
             </p>
